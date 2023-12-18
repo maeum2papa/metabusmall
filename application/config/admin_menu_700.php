8555c704a775d1e2515ad504a9b0e3444d6899ba
Kim Jae-Won <okhi1@naver.com>
2023-12-19 00:18:43 +0900

    슈퍼관리자 예치금관리 메뉴 수정

--- application/config/admin_menu_700.php
+++ application/config/admin_menu_700.php
@@ -15,10 +15,10 @@ $config['admin_page_menu']['deposit'] =
 	array(
 		'__config'					=> array('예치금관리', 'fa-money'),
 		'menu'						=> array(
-			'depositcfg'			=> array('예치금환경설정', ''),
-			'emailform'				=> array('메일/쪽지발송양식', ''),
-			'pendingbank'			=> array('무통장입금알림', ''),
+			// 'depositcfg'			=> array('예치금환경설정', ''),
+			// 'emailform'				=> array('메일/쪽지발송양식', ''),
+			// 'pendingbank'			=> array('무통장입금알림', ''),
 			'depositlist'			=> array('예치금변동내역', ''),
-			'depositstat'			=> array('예치금통계', ''),
+			// 'depositstat'			=> array('예치금통계', ''),
 		),
 	);
