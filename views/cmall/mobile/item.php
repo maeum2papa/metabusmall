dee6938bc9147f9aa91367d68fb2d840c819e8d6
Kim Jae-Won <okhi1@naver.com>
2023-12-22 06:57:18 +0900

    '

--- views/cmall/mobile/item.php
+++ views/cmall/mobile/item.php
@@ -1,8 +1,19 @@
 <?php $this->managelayout->add_css(element('view_skin_url', $layout) . '/css/style.css'); ?>
 <?php $this->managelayout->add_js(base_url('assets/js/cmallitem.js')); ?>
 
-<div class="market">
-	<h3>상품안내</h3>
+<div class="market" id="asmo_item_detail_wrap">
+	
+	<!-- 몰 상단 공통 요소  -->
+	<div class="cmall_top_wrap no_left">
+		<div class="top_right_box">
+			<a class="asmo_top_wish" href="/cmall/wishlist">찜하기목록으로</a>
+			<a class="asmo_top_cart_btn" href="/cmall/cart">장바구니<em></em></a>
+			<a class="asmo_top_order_list_btn" href="/cmall/orderlist">구매내역<em></em></a>
+		</div>
+	</div>
+	<!-- //몰 상단 공통 요소  -->
+
+	<!-- <h3>상품안내</h3> -->
 	<?php if ($this->member->is_admin()) { ?>
 		<a href="<?php echo admin_url('cmall/cmallitem/write/' . element('cit_id', element('data', $view))); ?>" target="_blank" class="btn-sm btn btn-danger pull-right">상품내용수정</a>
 	<?php } ?>
@@ -10,7 +21,7 @@
 		<div class="product-detail"><?php echo element('header_content', element('data', $view)); ?></div>
 	<?php } ?>
 	<div class="market">
-		<div class="product-box mb20">
+		<div class="product-box">
 			<div class="product-left">
 				<div class="item_slider">
 					<?php
@@ -19,7 +30,7 @@
 						continue;
 					}
 					?>
-						<div><img src="<?php echo thumb_url('cmallitem', element('cit_file_' . $i, element('data', $view)), 350, 350); ?>" alt="<?php echo html_escape(element('cit_name', element('data', $view))); ?>" title="<?php echo html_escape(element('cit_name', element('data', $view))); ?>" style="width:350px;height:350px;" onClick="window.open('<?php echo site_url('cmall/itemimage/' . html_escape(element('cit_key', element('data', $view)))); ?>', 'win_image', 'left=100,top=100,width=730,height=700,scrollbars=1');" /></div>
+						<div><img src="<?php echo thumb_url('cmallitem', element('cit_file_' . $i, element('data', $view))); ?>" alt="<?php echo html_escape(element('cit_name', element('data', $view))); ?>" title="<?php echo html_escape(element('cit_name', element('data', $view))); ?>" onClick="window.open('<?php echo site_url('cmall/itemimage/' . html_escape(element('cit_key', element('data', $view)))); ?>', 'win_image', 'left=100,top=100,width=730,height=700,scrollbars=1');" /></div>
 					<?php } ?>
 				</div>
 				<span class="prev" id="slider-prev"></span>
@@ -27,7 +38,8 @@
 			</div>
 			<div class="product-right">
 				<div class="product-title"><?php echo html_escape(element('cit_name', element('data', $view))); ?></div>
-				<table class="product-no table table-bordered">
+				<!-- asmo lhb 231221 상품정보 테이블 미노출  -->
+				<table class="product-no table table-bordered" style="display:none;">
 					<tbody>
 						<tr>
 							<td>상품코드</td>
@@ -64,91 +76,99 @@
 						</tr>
 					</tbody>
 				</table>
+
+				<!-- asmo lhb 231221 옵션 폼 위치 이동 -->
+				<?php
+				if (element('detail', element('data', $view))) {
+					$attributes = array('class' => 'form-horizontal', 'name' => 'fitem', 'id' => 'fitem', 'onSubmit' => 'return fitem_submit(this)');
+					echo form_open(current_full_url(), $attributes);
+				?>
+					<input type="hidden" name="stype" id="stype" value="" />
+					<input type="hidden" name="cit_id" value="<?php echo element('cit_id', element('data', $view)); ?>" />
+					<div class="product-option">
+						<table class="table table-bordered item_detail_table">
+							<tbody>
+								<tr class="danger" style="display:none;">
+									<th style="display:none;"><input type="checkbox" id="chk_all_item" /></th>
+									<th>수량</th>
+									<th>판매가</th>
+								</tr>
+								<?php
+								foreach (element('detail', element('data', $view)) as $detail) {
+									$price = element('cit_price', element('data', $view)) + element('cde_price', $detail);
+								?>
+									<tr>
+										<td class="asmo_opt_chk_td" style="display:none;"><input type="checkbox" name="chk_detail[]" value="<?php echo element('cde_id', $detail); ?>" /></td>
+										<td colspan="2">
+											<!-- asmo lhb 231221 옵션명 위치 이동  -->
+											<div class="asmo_opt_tit_td_box">
+												<?php echo html_escape(element('cde_title', $detail)); ?>
+											</div>
+											<!-- //asmo lhb 231221 옵션명 위치 이동  -->
+											<div class="btn-group asmo_cnt" role="group" aria-label="...">
+												<button type="button" class="btn btn-default btn-xs btn-change-qty" data-change-type="minus">-</button>
+												<input type="text" name="detail_qty[<?php echo element('cde_id', $detail); ?>]" class="btn btn-default btn-xs detail_qty" value="1" />
+												<button type="button" class="btn btn-default btn-xs btn-change-qty" data-change-type="plus">+</button>
+											</div>
+										</td>
+										<td class="detail_price">
+											<input type="hidden" name="item_price[<?php echo element('cde_id', $detail); ?>]" value="<?php echo $price; ?>" />
+											<?php echo number_format($price); ?>원
+										</td>
+									</tr>
+								<?php } ?>
+								<tr class="success">
+									<td colspan="2">총 결제 열매</td>
+									<td class="cart_total_price"><span id="total_order_price">0</span>원</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<div class="asmo_item_btn_wrap">
+						<button type="submit" onClick="$('#stype').val('order');" class="btn btn-black">구매하기</button>
+						<div class="asmo_item_btn_bottom_wrap">
+							<button type="submit" onClick="$('#stype').val('cart');"class="btn btn-black">장바구니</button>
+							<button type="submit" onClick="$('#stype').val('wish');"class="btn btn-black">찜하기</button>
+							<button>목록으로</button>
+						</div>
+					</div>
+				<?php
+					echo form_close();
+				}
+				?>
+				<!-- //asmo lhb 231221 옵션 폼 위치 이동 -->
 			</div>
 		</div>
 
-		<?php
-		if (element('detail', element('data', $view))) {
-			$attributes = array('class' => 'form-horizontal', 'name' => 'fitem', 'id' => 'fitem', 'onSubmit' => 'return fitem_submit(this)');
-			echo form_open(current_full_url(), $attributes);
-		?>
-			<input type="hidden" name="stype" id="stype" value="" />
-			<input type="hidden" name="cit_id" value="<?php echo element('cit_id', element('data', $view)); ?>" />
-			<div class="product-option">
-				<table class="table table-bordered item_detail_table">
-					<tbody>
-						<tr class="danger">
-							<th>옵션</th>
-							<th><input type="checkbox" id="chk_all_item" /></th>
-							<th>수량</th>
-							<th>판매가</th>
-						</tr>
-						<?php
-						foreach (element('detail', element('data', $view)) as $detail) {
-							$price = element('cit_price', element('data', $view)) + element('cde_price', $detail);
-						?>
-							<tr>
-								<td><?php echo html_escape(element('cde_title', $detail)); ?></td>
-								<td><input type="checkbox" name="chk_detail[]" value="<?php echo element('cde_id', $detail); ?>" /></td>
-								<td>
-									<div class="btn-group" role="group" aria-label="...">
-										<button type="button" class="btn btn-default btn-xs btn-change-qty" data-change-type="minus">-</button>
-										<input type="text" name="detail_qty[<?php echo element('cde_id', $detail); ?>]" class="btn btn-default btn-xs detail_qty" value="1" />
-										<button type="button" class="btn btn-default btn-xs btn-change-qty" data-change-type="plus">+</button>
-									</div>
-								</td>
-								<td class="detail_price">
-									<input type="hidden" name="item_price[<?php echo element('cde_id', $detail); ?>]" value="<?php echo $price; ?>" />
-									<?php echo number_format($price); ?>원
-								</td>
-							</tr>
-						<?php } ?>
-						<tr class="success">
-							<td>총금액</td>
-							<td></td>
-							<td></td>
-							<td class="cart_total_price"><span id="total_order_price">0</span>원</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<div class="pull-right mb20 mt20">
-				<button type="submit" onClick="$('#stype').val('order');" class="btn btn-black">바로구매</button>
-				<button type="submit" onClick="$('#stype').val('cart');"class="btn btn-black">장바구니</button>
-				<button type="submit" onClick="$('#stype').val('wish');"class="btn btn-black">찜하기</button>
-			</div>
-		<?php
-			echo form_close();
-		}
-		?>
-		<div class="product-info mb20">
-			<ul class="product-info-top" id="itemtabmenu1">
-				<li class="current"><a href="#itemtabmenu1">상품정보</a></li>
+		
+		<div class="product-info asmo_item_info_common_box">
+			<ul class="product-info-top asmo_common_item_tab" id="itemtabmenu1">
+				<li class="current"><a href="#itemtabmenu1">상세설명</a></li>
 				<li><a href="#itemtabmenu2">사용후기 <span class="item_review_count"><?php echo number_format(element('cit_review_count', element('data', $view)));?></span></a></li>
 				<li><a href="#itemtabmenu3">상품문의 <span class="item_qna_count"><?php echo number_format(element('cit_qna_count', element('data', $view)));?></span></a></li>
 			</ul>
-			<div class="product-detail"><?php echo element('content', element('data', $view)); ?></div>
+			<div class="product-detail asmo_item_common_content_box"><?php echo element('content', element('data', $view)); ?></div>
 		</div>
-		<div class="product-info mb20">
-			<ul class="product-info-top" id="itemtabmenu2">
-				<li><a href="#itemtabmenu1">상품정보</a></li>
+		<div class="product-info asmo_item_info_common_box">
+			<ul class="product-info-top asmo_common_item_tab" id="itemtabmenu2">
+				<li><a href="#itemtabmenu1">상세설명</a></li>
 				<li class="current"><a href="#itemtabmenu2">사용후기 <span class="item_review_count"><?php echo number_format(element('cit_review_count', element('data', $view)));?></span></a></li>
 				<li><a href="#itemtabmenu3">상품문의 <span class="item_qna_count"><?php echo number_format(element('cit_qna_count', element('data', $view)));?></span></a></li>
 			</ul>
-			<div id="viewitemreview"></div>
-			<div class="pull-right mb20 mr20">
-				<a href="javascript:;" class="btn btn-default btn-sm" onClick="window.open('<?php echo site_url('cmall/review_write/' . element('cit_id', element('data', $view))); ?>', 'review_popup', 'width=750,height=770,scrollbars=1'); return false;">사용후기 쓰기</a>
+			<div id="viewitemreview" class="asmo_item_common_content_box asmo_item_board_list"></div>
+			<div class="asmo_item_common_write_btn">
+				<a href="javascript:;" class="btn btn-default btn-sm" onClick="window.open('<?php echo site_url('cmall/review_write/' . element('cit_id', element('data', $view))); ?>', 'review_popup', 'width=750,height=770,scrollbars=1'); return false;">작성하기<em></em></a>
 			</div>
 		</div>
-		<div class="product-info mb20">
-			<ul class="product-info-top" id="itemtabmenu3">
-				<li><a href="#itemtabmenu1">상품정보</a></li>
+		<div class="product-info asmo_item_info_common_box">
+			<ul class="product-info-top asmo_common_item_tab" id="itemtabmenu3">
+				<li><a href="#itemtabmenu1">상세설명</a></li>
 				<li><a href="#itemtabmenu2">사용후기 <span class="item_review_count"><?php echo number_format(element('cit_review_count', element('data', $view)));?></span></a></li>
 				<li class="current"><a href="#itemtabmenu3">상품문의 <span class="item_qna_count"><?php echo number_format(element('cit_qna_count', element('data', $view)));?></span></a></li>
 			</ul>
-			<div id="viewitemqna"></div>
-			<div class="pull-right mb20 mr20">
-				<a href="javascript:;" class="btn btn-sm btn-default" onClick="window.open('<?php echo site_url('cmall/qna_write/' . element('cit_id', element('data', $view))); ?>', 'qna_popup', 'width=750,height=770,scrollbars=1'); return false;">상품문의 쓰기</a>
+			<div id="viewitemqna" class="asmo_item_common_content_box asmo_item_board_list"></div>
+			<div class="asmo_item_common_write_btn">
+				<a href="javascript:;" class="btn btn-sm btn-default" onClick="window.open('<?php echo site_url('cmall/qna_write/' . element('cit_id', element('data', $view))); ?>', 'qna_popup', 'width=750,height=770,scrollbars=1'); return false;">작성하기<em></em></a>
 			</div>
 		</div>
 		<?php if (element('footer_content', element('data', $view))) { ?><div class="product-detail"><?php echo element('footer_content', element('data', $view)); ?></div><?php } ?>
@@ -157,13 +177,24 @@
 
 <script type="text/javascript" src="<?php echo base_url('assets/js/bxslider/jquery.bxslider.min.js'); ?>"></script>
 <script type="text/javascript">
+		
+//asmo lhb 231218 클래스 영역 구분용 클래스 추가
+document.querySelector('.main').classList.add('asmo_m_layout');
+
+
+//asmo lhb 231221 옵션 체크박스 체크 시키기
+$('.product-option .asmo_opt_chk_td input[type="checkbox"]').trigger('click');
+$('.product-option .asmo_opt_chk_td input[type="checkbox"]').prop('checked',true);
+//asmo lhb 231221 옵션 체크박스 체크 시키기 끝
+
+
 //<![CDATA[
 $('.item_slider').bxSlider({
 	pager : false,
 	nextSelector: '#slider-next',
 	prevSelector: '#slider-prev',
-	nextText: '<img src="<?php echo element('view_skin_url', $layout); ?>/images/btn_next.png" alt="다음" title="다음" />',
-	prevText: '<img src="<?php echo element('view_skin_url', $layout); ?>/images/btn_prev.png" alt="이전" title="이전" />'
+	nextText: '<img src="<?php echo element('layout_skin_url', $layout); ?>/images/arrow_R.svg" alt="다음" title="다음" />',
+	prevText: '<img src="<?php echo element('layout_skin_url', $layout); ?>/images/arrow_L.svg" alt="이전" title="이전" />'
 });
 $(document).ready(function($) {
 	view_cmall_review('viewitemreview', '<?php echo element('cit_id', element('data', $view)); ?>', '', '');
